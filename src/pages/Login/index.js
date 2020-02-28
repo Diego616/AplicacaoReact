1e5fac44d0a89518e50ff36307c0b59dd556a5a9
Diego <diego.henri.oliveira@live.com>
2020-02-28 12:48:29 -0300

    iniciando projeto

---
+++ src/pages/Login/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled';
+import * as exampleActions from '../../store/modules/example/actions';
+
+export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch(exampleActions.clicaBotaoRequest());
+  }
+
+  return (
+    <Container>
+      <h1>Login</h1>
+    </Container>
+  );
+}
